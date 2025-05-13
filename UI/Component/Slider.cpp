d41b55d86711aa1bff0204164ab68ed99ef4fcfd
Kaelyn <105290519+KaelynSun@users.noreply.github.com>
2025-05-13 16:36:29 +0800

    Update Slider.cpp

--- UI/Component/Slider.cpp
+++ UI/Component/Slider.cpp
@@ -14,13 +14,24 @@ Slider::Slider(float x, float y, float w, float h)
     Anchor = Engine::Point(0.5, 0.5);
 }
 void Slider::Draw() const {
-    // TODO HACKATHON-5 (3/4): The slider's component should be drawn here.
+    // TODO HACKATHON-5 (3/4): The slider's component should be drawn here. [DONE]
+    Bar.Draw();
+    End1.Draw();
+    End2.Draw();
+    ImageButton::Draw();
 }
 void Slider::SetOnValueChangedCallback(std::function<void(float value)> onValueChangedCallback) {
     OnValueChangedCallback = onValueChangedCallback;
 }
 void Slider::SetValue(float value) {
-    // TODO HACKATHON-5 (4/4): Set the value of the slider and call the callback.
+    // TODO HACKATHON-5 (4/4): Set the value of the slider and call the callback. [DONE]
+    value = std::clamp(value, 0.0f, 1.0f); // Ensure value is between 0-1
+    Position.x = Bar.Position.x + value * Bar.Size.x; // Update handle position
+    
+    // Call the callback if it exists
+    if (OnValueChangedCallback) {
+        OnValueChangedCallback(value);
+    }
 }
 void Slider::OnMouseDown(int button, int mx, int my) {
     if ((button & 1) && mouseIn)
