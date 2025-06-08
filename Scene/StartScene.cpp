95b45e1df13c2fe8d5225429fa4d85f2730c7162
Kaelyn <105290519+KaelynSun@users.noreply.github.com>
2025-06-09 00:42:26 +0800

    Update StartScene.cpp

--- Scene/StartScene.cpp
+++ Scene/StartScene.cpp
@@ -42,7 +42,7 @@ void StartScene::Terminate() {
     IScene::Terminate();
 }
 void StartScene::PlayOnClick(int stage) {
-    Engine::GameEngine::GetInstance().ChangeScene("stage-select");
+    Engine::GameEngine::GetInstance().ChangeScene("login");
 }
 void StartScene::SettingsOnClick(int stage) {
     Engine::GameEngine::GetInstance().ChangeScene("settings");
