9da3b05ad6582d72c8710834ebcf98f05ad10db6
Kaelyn <105290519+KaelynSun@users.noreply.github.com>
2025-06-11 12:04:28 +0800

    Update Enemy.hpp

--- Enemy/Enemy.hpp
+++ Enemy/Enemy.hpp
@@ -15,6 +15,7 @@ class Enemy : public Engine::Sprite {
 protected:
     std::vector<Engine::Point> path;
     float speed;
+    float maxHP;
     float hp;
     int money;
     PlayScene *getPlayScene();
@@ -27,6 +28,9 @@ public:
     std::list<Turret *> lockedTurrets;
     std::list<Bullet *> lockedBullets;
     Enemy(std::string img, float x, float y, float radius, float speed, float hp, int money);
+    float GetHealthPercentage() const { return hp / maxHP; }
+    std::string GetHealthText() const { return std::to_string((int)hp) + "/" + std::to_string((int)maxHP); }
+    float getMaxHP() const { return maxHP; }
     float getHP() const { return hp; }
     void setHP(float value) { hp = value; }
     void Hit(float damage, bool isAOE);
