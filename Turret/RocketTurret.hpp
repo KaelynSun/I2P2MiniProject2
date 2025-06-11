0698d132d86269a6287e2c1003daeabb6059abcb
Wri0thesl4y <mvsfeliceangeline@gmail.com>
2025-06-11 13:15:23 +0800

    Update RocketTurret.hpp

--- Turret/RocketTurret.hpp
+++ Turret/RocketTurret.hpp
@@ -7,5 +7,11 @@ public:
     static const int Price;
     RocketTurret(float x, float y);
     void CreateBullet() override;
+    void Upgrade() override {
+        //Turret::Upgrade();
+        atk *= 1.5f;
+        hp *= 1.5f;
+    }
+    std::string GetName() const override { return "Rocket Turret"; }
 };
 #endif   // MACHINEGUNTURRET_HPP
