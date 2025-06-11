6a83d355560f461ee91c9556f53e9ad9375ef09c
Wri0thesl4y <mvsfeliceangeline@gmail.com>
2025-06-11 13:14:24 +0800

    Update LaserTurret.hpp

--- Turret/LaserTurret.hpp
+++ Turret/LaserTurret.hpp
@@ -7,5 +7,11 @@ public:
     static const int Price;
     LaserTurret(float x, float y);
     void CreateBullet() override;
+    void Upgrade() override {
+        //Turret::Upgrade();
+        atk *= 1.5f;
+        hp *= 1.5f;
+    }
+    std::string GetName() const override { return "Laser Turret"; }
 };
 #endif   // LASERTURRET_HPP
