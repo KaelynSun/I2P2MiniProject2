5f08af751639c74a4af761bc990cd8c2f0bfbe4b
Wri0thesl4y <mvsfeliceangeline@gmail.com>
2025-06-12 15:02:17 +0800

    Update Sprite.hpp

--- Engine/Sprite.hpp
+++ Engine/Sprite.hpp
@@ -43,7 +43,7 @@ namespace Engine {
         /// Draw to window display.
         /// This is called when the game should redraw the window.
         /// </summary>
-        void Draw() const override;
+        virtual void Draw() const override;
         /// <summary>
         /// Run game logic such as updating the world, checking for collision, and so on.
         /// This is called when the game should update its logic, usually 'fps' times per second.
