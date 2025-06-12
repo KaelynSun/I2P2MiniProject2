85f5e8f3f5f1cc8a1043bd6f84013f7b25791518
Wri0thesl4y <mvsfeliceangeline@gmail.com>
2025-06-12 15:01:15 +0800

    Update PlaneEnemy.cpp

--- Enemy/PlaneEnemy.cpp
+++ Enemy/PlaneEnemy.cpp
@@ -4,4 +4,8 @@
 
 // TODO HACKATHON-3 (1/3): You can imitate the 2 files: 'SoldierEnemy.hpp', 'SoldierEnemy.cpp' to create a new enemy.
 PlaneEnemy::PlaneEnemy(int x, int y) : Enemy("play/enemy-2.png", x, y, 10, 50, 5, 5) {
+    bulletImage = "play/bullet-9.png"; // Set the bullet image for this enemy
+    attackRange = 400; 
+    attackDamage = 1; 
+    attackCooldown = 0.5f; 
 }
