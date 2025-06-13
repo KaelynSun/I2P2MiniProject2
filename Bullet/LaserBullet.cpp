30e77c70a3dd9a68533630afb5305cdc6a304930
Wri0thesl4y <mvsfeliceangeline@gmail.com>
2025-06-13 22:55:40 +0800

    Update LaserBullet.cpp

--- Bullet/LaserBullet.cpp
+++ Bullet/LaserBullet.cpp
@@ -12,6 +12,7 @@
 class Turret;
 
 LaserBullet::LaserBullet(Engine::Point position, Engine::Point forwardDirection, float rotation, Turret *parent) : Bullet("play/bullet-2.png", 800, 2, position, forwardDirection, rotation - ALLEGRO_PI / 2, parent) {
+    damage = 20;
 }
 void LaserBullet::OnExplode(Enemy *enemy) {
     std::random_device dev;
