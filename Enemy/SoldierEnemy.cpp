ed46fe1daac5aeb5229a91828501017a22d8da84
Wri0thesl4y <mvsfeliceangeline@gmail.com>
2025-06-13 22:56:54 +0800

    Update SoldierEnemy.cpp

--- Enemy/SoldierEnemy.cpp
+++ Enemy/SoldierEnemy.cpp
@@ -3,9 +3,9 @@
 #include "SoldierEnemy.hpp"
 
 // TODO HACKATHON-3 (1/3): You can imitate the 2 files: 'SoldierEnemy.hpp', 'SoldierEnemy.cpp' to create a new enemy.
-SoldierEnemy::SoldierEnemy(int x, int y) : Enemy("play/enemy-1.png", x, y, 10, 50, 5, 5) {
+SoldierEnemy::SoldierEnemy(int x, int y) : Enemy("play/enemy-1.png", x, y, 10, 50, 50, 20) {
     bulletImage = "play/bullet-8.png"; // Set the bullet image for this enemy
     attackRange = 200; 
-    attackDamage = 1; 
-    attackCooldown = 1.5f; 
+    attackDamage = 5; 
+    attackCooldown = 1.0f; 
 }
