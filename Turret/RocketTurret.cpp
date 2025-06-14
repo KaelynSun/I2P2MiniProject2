8de6824b206fd5eddf227a9d0301b172eab263f8
Wri0thesl4y <mvsfeliceangeline@gmail.com>
2025-06-14 14:12:06 +0800

    Update RocketTurret.cpp

--- Turret/RocketTurret.cpp
+++ Turret/RocketTurret.cpp
@@ -11,7 +11,7 @@
 
 const int RocketTurret::Price = 200;
 RocketTurret::RocketTurret(float x, float y)
-    : Turret("play/tower-base.png", "play/turret-6.png", x, y, 500, Price, 2.0) {
+    : Turret("play/tower-base.png", "play/turret-6.1.png", x, y, 500, Price, 2.0) {
         maxLifetime = 50.0f;
     // Move center downward, since we the turret head is slightly biased upward.
     Anchor.y += 8.0f / GetBitmapHeight();
